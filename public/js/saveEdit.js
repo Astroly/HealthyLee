07f5fd2c4e81f518745d98febb7b323cbb6c5d32
[Your <nitipon000@gmail.com>
2018-10-07 09:39:15 +0700

    ...

---
+++ public/js/saveEdit.js
@@ -0,0 +1,8 @@
+function saveEdit(){
+    nextPage();
+  alert('Success back to Profile');
+
+}
+function nextPage(){
+window.location.href="userProfile.html";
+}
---
+++ public/js/savePayment.js
@@ -0,0 +1,8 @@
+function savePayment(){
+      nextPage();
+    alert('Success back to Profile');
+  
+}
+function nextPage(){
+window.location.href="userProfile.html";
+ }
